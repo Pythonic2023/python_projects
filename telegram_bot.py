a339abc59ce3dd22efdbd7e0d11645d9f36c2fd7
Pythonic2023 <daltonfaust7295@gmail.com>
2024-04-27 08:41:19 -0400

    weatherapp.py now sends messages to bot via telegram api. Added code for communicating with telegram bot.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@ import requests
 
 chat_id = "chat_id"
 token = "token"
-msg = "kdlkdldkl"
+msg = "kdlkdldk"
 message = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={msg}"
 
 response = requests.get(message)
